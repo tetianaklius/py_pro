e5b84fe75aa94f6d0e455891374a4af58898a297
tetianaklius <90043120+tetianaklius@users.noreply.github.com>
2022-12-18 01:01:28 +0200

    Delete order directory

--- order/customer_.py
+++
@@ -1,9 +0,0 @@
-class Customer:
-    def __init__(self, name: str, age: str, phone_number: str, city: str):
-        self.name = name
-        self.age = age
-        self.phone_number = phone_number
-        self.city = city
-
-    def __str__(self):
-        return f"{self.name}, {self.age} years old, {self.city}, phone number: {self.phone_number}"
